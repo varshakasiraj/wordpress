a55c8f8cd82710173fafd84854d9df84eee69a88
varshakasiraj <116131392+varshakasiraj@users.noreply.github.com>
2023-08-24 18:20:52 +0530

    adjust admin table filter categories

---
+++ wp-content/themes/custometheme/assets/js/scripts.js
@@ -0,0 +1,116 @@
+var sb = sb || {};
+//Core scripts
+sb.core = function () {
+    var self = {
+        load: function () {
+            jQuery(document).ready(self.ready);
+        },
+        ready: function () {
+            self.loadtable();
+            self.tablesearch();
+            self.tablesort();
+            self.fliterCategories();
+            self.tablerefresh();
+            self.tablepagination();
+        },
+
+        loadtable:function(){
+            $('#sortmenu').append('<input id="filterbar" type="text" placeholder="Search..">');
+            
+            $('#mytable').append('<table id="myadmintable">');
+            $('#myadmintable').append('<tr>'
+            +'<th>ID</th>'
+            +'<th>'+'Post_categories_name'+'</th>'
+            +'<th>'+'Post_title'+'</th>'
+            +'<th>'+'Post_content'+'</th>'
+            +'<th>'+'Post_date'+'</th>'
+            +'<th>'+'Post_status'+'</th>'
+            +'</tr>');
+            $('#mytable').append('</table>');
+            $('#mytable').append('<input type="hidden" id="name_order" value="asc"></input>');
+        },
+        loaddata:function(){
+           
+            var productDataUrl = 'http://localhost/wordpress/wp-json/sb_clothes/v1/clothes/';
+            jQuery.ajax({
+                type: "GET",
+                url: productDataUrl,
+                success: function (response) {
+                    var postlist  = JSON.parse(response);
+            
+                        $('#myadmintable').append("<tbody id='myadmintbody'>");
+                        var  myArray = new Array();
+                        postlist.forEach(function(data){
+                            $('#myadmintable').append('<tr class="tablerow" data-id="'+data.post_term_id+'">'
+                            +'<td>'+data.post_id+'</td>'
+                            +'<td>'+data.post_term_name+'</td>'
+                            +'<td class="title">'+data.post_title+'</td>'
+                            +'<td>'+data.post_content+'</td>'
+                            +'<td>'+data.post_date+'</td>'
+                            +'<td>'+data.post_status+'</td>'
+                            +'</tr>');
+                            if (myArray[data.post_term_id] === undefined) {
+                                var term_id = data.post_term_id;
+                                var term_name = data.post_term_name;
+                                myArray[term_id] =[term_name];
+                            } 
+                         });                   
+                        $('#myadmintable').append("</tbody>");
+                        myArray.forEach(function(value,key){ 
+                            $('#filtercheckbox').append('<li class = "filtercheckboxlist"><input type="checkbox" name="' +key+ '" id="' +key+ '"/>' +
+                             '<label for="' +value + '">' + value +'</label></li>');
+                             self.fliterCategories(key,value);
+                        });
+                        $('#filtercheckbox').append('</ul>');
+                        
+                   
+                }
+            });
+        },
+        tablesearch:function(){
+            $('#searchbar').on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $('#myadmintbody  tr').filter(function(){
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                })
+            });
+        },
+        fliterCategories:function(key,value){
+            $('#'+key).change(function() {
+            var count = 0 ;
+            if($('#'+key).is(':checked')){
+               $('#myadmintbody  tr').filter(function(){
+                 $(this).toggle($(this).attr("data-id").indexOf(key) > -1);
+                 $('#'+key).parent().css('color', 'green');
+                count =$('.tablerow:not([style*="display: none"])').length;    
+             })
+              $('<p>'+value +' count '+ count +'<p>').insertAfter("#filtercheckbox");
+           }
+        });
+        },
+        tablesort:function(){
+            $('#sorttable').click(function(){
+                var tbody =$('#myadmintbody');
+                tbody.find('tr').sort(function(a, b){
+                if($('#name_order').val()=='asc'){
+                    return $('td:eq(2)', a).text().localeCompare($('td:eq(2)', b).text());
+                }
+                else 
+                {
+                    return $('td:eq(2)', b).text().localeCompare($('td:eq(2)', a).text());
+                }
+                    
+            }).appendTo(' #mytable #myadmintbody'); 
+            });
+           
+        },
+        tablerefresh:function(){
+            $('#refreshtable').click(function(){
+                history.go(0);
+            });
+            
+        },
+    };
+    return self;
+}();
+sb.core.load();
---
+++ wp-content/themes/custometheme/classes/postapi.php
@@ -0,0 +1,48 @@
+<?php
+class CustomeApi{
+    
+    public static function  register_api(){
+        add_action('rest_api_init',array(get_called_class(),'register_api_routes'));
+    }
+    public static function register_api_routes(){
+        register_rest_route( 'sb_clothes/v1' , '/clothes/', array(
+            'methods' => 'GET',
+            'callback' => array(get_called_class(),'clothes_posts'),
+        ));
+    }
+    public static function clothes_posts(){
+            $args = array(
+                'post_type' => 'clothes',
+                'post_status' => 'publish',
+                'posts_per_page' => -1,
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'men',
+                        'field' => 'slug',
+                        'terms' => array('casual','formal','traditional','frock','maxi','women','footwear' ),
+                        
+                    ),
+                )
+                
+            );
+            $clothes = get_posts( $args );
+            foreach ($clothes as $row) {
+                $men_categories = get_the_terms($row->ID, 'men' );
+                foreach($men_categories as $men){
+                     $dress[] = array(
+                        "post_id" => $row->ID,
+                        "post_title"=>$row->post_title,
+                        "post_content" =>$row->post_content,
+                        "post_date"=>$row->post_date,
+                        "post_status"=>$row->post_status, 
+                        "post_term_name" => $men->name,
+                        "post_term_id" => $men->term_id
+                     );
+                    
+                }
+           }   
+        return  json_encode($dress);           
+    }
+}
+CustomeApi::register_api();
+?>
